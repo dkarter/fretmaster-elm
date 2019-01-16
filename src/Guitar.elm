d3c7e8409bc7765da233e09c0f20713efc7d70c1
Dorian Karter <dkarter@users.noreply.github.com>
2019-01-16 02:45:36 -0600

    Space out frets more linearly
    This is not an accurate depiction but better than uniform sizes for all
    frets.
    
    I consulted this resource originally:
    https://www.liutaiomottola.com/formulae/fret.htm
    But ended up coming up with my own formulae.

--- src/Guitar.elm
+++ src/Guitar.elm
@@ -4,6 +4,7 @@ module Guitar exposing
     , findAllOctaves
     , getGuitarNoteName
     , getGuitarStringName
+    , guitarStrings
     , isMarkerFret
     )
 
--- src/components/Fretboard.elm
+++ src/components/Fretboard.elm
@@ -2,10 +2,19 @@ module Fretboard exposing (render)
 
 import Guitar
 import Html exposing (Html, button, div, h1, img, input, label, span, text)
-import Html.Attributes exposing (checked, class, classList, src, type_)
+import Html.Attributes exposing (checked, class, classList, src, style, type_)
 import Html.Events exposing (onCheck, onClick)
 import Model exposing (Model)
 import Msg exposing (Msg(..))
+
+
+fretCount =
+    12
+
+
+stringCount =
+    Guitar.guitarStrings
+        |> List.length
 
 
 isSelected : Model -> Int -> Int -> Bool
@@ -34,8 +43,34 @@ isOctave model stringNum fretNum =
 renderFrets : Model -> Int -> List (Html Msg)
 renderFrets model stringNum =
     let
+        fretWidth fretNum =
+            let
+                minFretSize =
+                    4
+
+                reverseFretNum =
+                    (fretCount + 1) - toFloat fretNum
+
+                scale =
+                    2
+            in
+            minFretSize + (reverseFretNum / scale)
+
+        fretWidthInRems fretNum =
+            fretNum
+                |> fretWidth
+                |> String.fromFloat
+                |> List.singleton
+                |> List.append [ "rem" ]
+                |> List.reverse
+                |> String.concat
+                |> Debug.log "fret width"
+
         renderFret fretNum =
-            div [ classList [ ( "fret", True ), ( "fret-marker", Guitar.isMarkerFret fretNum stringNum ) ] ]
+            div
+                [ classList [ ( "fret", True ), ( "fret-marker", Guitar.isMarkerFret fretNum stringNum ) ]
+                , style "width" (fretWidthInRems fretNum)
+                ]
                 [ div
                     [ class "string-line"
                     , classList
@@ -47,13 +82,13 @@ renderFrets model stringNum =
                     []
                 ]
     in
-    List.range 1 12
+    List.range 1 fretCount
         |> List.map renderFret
 
 
 renderStrings : Model -> List (Html Msg)
 renderStrings model =
-    List.range 1 6
+    List.range 1 stringCount
         |> List.map (renderString model)
 
 
