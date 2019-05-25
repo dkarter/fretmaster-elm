127960217e03fddbab04b890bea5575b86e62636
Dorian Karter <dkarter@users.noreply.github.com>
2019-05-25 02:53:08 -0500

    Calculate scales and modes

--- src/Music.elm
+++ src/Music.elm
@@ -1,15 +1,92 @@
-module Music exposing (Note, PitchNotation, getNoteNameByIndex, notes, pitchNotationToStr)
+module Music exposing (Note, PitchNotation, ScaleType(..), getNoteNameByIndex, getScaleNotes, notes, pitchNotationToStr)
 
 import Array
+import List.Extra
 import Maybe exposing (withDefault)
+import Utils
 
 
 type alias Note =
     String
 
 
+type alias Interval =
+    String
+
+
 type alias PitchNotation =
     ( String, Int )
+
+
+type ScaleType
+    = Major
+    | Minor
+    | Dorian
+    | Phrygian
+    | Lydian
+    | Mixolydian
+      -- | Locrian
+    | HarmonicMinor
+    | MelodicMinor
+    | MajorPentatonic
+    | MinorPentatonic
+
+
+scaleFormula : ScaleType -> List Interval
+scaleFormula scaleType =
+    case scaleType of
+        Major ->
+            [ "1", "2", "3", "4", "5", "6", "7" ]
+
+        Minor ->
+            [ "1", "2", "b3", "4", "5", "b6", "b7" ]
+
+        Dorian ->
+            [ "1", "2", "b3", "4", "5", "6", "b7" ]
+
+        Phrygian ->
+            [ "1", "b2", "b3", "4", "5", "b6", "b7" ]
+
+        Lydian ->
+            [ "1", "2", "3", "#4", "5", "6", "7" ]
+
+        Mixolydian ->
+            [ "1", "2", "3", "4", "5", "6", "b7" ]
+
+        -- Locrian ->
+        --     [ "1", "b2", "b3", "4", "b5", "b6", "b7" ]
+        MajorPentatonic ->
+            [ "1", "2", "3", "5", "6" ]
+
+        MinorPentatonic ->
+            [ "1", "b3", "4", "5", "b7" ]
+
+        _ ->
+            -- not implemented yet
+            []
+
+
+intervalToInt : Interval -> Int
+intervalToInt interval =
+    [ "1", "b2", "2", "b3", "3", "4", "#4", "5", "b6", "6", "b7", "7" ]
+        |> List.Extra.elemIndex interval
+        |> Maybe.withDefault -1
+
+
+getScaleNotes : Note -> ScaleType -> List Note
+getScaleNotes key scaleType =
+    let
+        formula =
+            scaleFormula scaleType
+
+        keyIndex =
+            getIndexByNoteName key
+
+        intervalToNote interval =
+            getNoteNameByIndex (Utils.getWrappedIndex notes (intervalToInt interval + keyIndex))
+    in
+    formula
+        |> List.map intervalToNote
 
 
 notes : List Note
@@ -35,6 +112,16 @@ pitchNotationToStr spn =
     in
     [ normalizedNoteName, pitchString ]
         |> String.join ""
+
+
+getIndexByNoteName : Note -> Int
+getIndexByNoteName note =
+    let
+        index =
+            notes
+                |> List.Extra.elemIndex note
+    in
+    withDefault -1 index
 
 
 getNoteNameByIndex : Int -> Note
